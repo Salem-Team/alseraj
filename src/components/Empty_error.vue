549e4329ae22e9ec072cb0a5e6f053125b32036f
Salem-Team <salemteamcompany@gmail.com>
2024-07-22 08:52:34 +0300

    first commit

---
+++ src/components/Empty_error.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- Display a row with a message when there is no data -->
+    <v-row>
+        <v-col>
+            <div class="m-auto text-center pa-auto">
+                <p
+                    style="
+                        color: var(--main-color);
+                        background-color: var(--secound-color);
+                        font-size: 20px;
+                        font-weight: bold;
+                        width: 90%;
+                        padding: 10px;
+                        border-redius: 5px;
+                        margin: auto;
+                        text-align: center;
+                    "
+                >
+                    {{ text }}
+                </p>
+            </div>
+        </v-col>
+    </v-row>
+</template>
+<script>
+export default {
+    props: ["text"],
+    data() {
+        return {};
+    },
+};
+</script>

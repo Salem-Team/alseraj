2c199c0c8469ee6ceda80c22a945186983827fad
Eslam Abosife <156938103+eslam1abosife@users.noreply.github.com>
2024-07-24 17:17:49 +0300

    start33

--- src/components/confirm_message.vue
+++ src/components/confirm_message.vue
@@ -1,7 +1,6 @@
 <template>
     <v-snackbar v-model="snackbar" timeout="3000">
         {{ text }}
-
         <template v-slot:actions>
             <v-btn
                 color="var(--main-color)"
